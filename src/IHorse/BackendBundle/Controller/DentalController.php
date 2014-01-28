7001416da2610ba10ea168500918fd9a17d03b5c
josegargon <josegargon2@gmail.com>
2014-01-28 09:52:40 +0100

    [Sprint 50] Dental file crud

---
+++ src/IHorse/BackendBundle/Controller/DentalController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace IHorse\BackendBundle\Controller;
+
+use IHorse\BackendBundle\Controller\IHorseController;
+use IHorse\BackendBundle\Form\DentalType;
+use Symfony\Component\HttpFoundation\Request;
+
+class DentalController extends IHorseController
+{
+    public function listDentalAction($horse)
+    {
+        $session = $this->getRequest()->getSession();
+        $horse = $this->get('rest.handler.model')->get('horses/'.$horse, 'horse', $session->get('access_token'));
+        $dentals = $this->get('dental.handler.model')->getDentals($horse, $session->get('access_token'));
+        ldd($dentals);
+        return $this->render('BackendBundle:Dental:view.html.twig', array('dental' => $dentals[0], 'horse' => $horse));
+    }
+
+    public function dentalShowAction($horse, $id)
+    {
+        $session = $this->getRequest()->getSession();
+        $dental = $this->get('dental.handler.model')->getDental($horse, $id, $session->get('access_token'));
+        $horse = $this->get('rest.handler.model')->get('horses/'.$horse, 'horse', $session->get('access_token'));
+
+        return $this->render('BackendBundle:Dental:view.html.twig', array('dental' => $dental, 'horse' => $horse));
+    }
+
+    public function newDentalAction($horse)
+    {
+        $session = $this->getRequest()->getSession();
+        $form = $this->createForm(new DentalType());
+        $horse = $this->get('rest.handler.model')->get('horses/'.$horse, 'horse', $session->get('access_token'));
+
+
+        return $this->render('BackendBundle:Dental:create.html.twig', array('form' => $form->createView(), 'horse' => $horse));
+    }
+
+    public function editDentalAction($horse, $id)
+    {
+        $session = $this->getRequest()->getSession();
+        $horse = $this->get('rest.handler.model')->get('horses/'.$horse, 'horse', $session->get('access_token'));
+        $dental = $this->get('dental.handler.model')->getDental($horse, $id, $session->get('access_token'));
+        $form = $form = $this->createForm(new DentalType(), $dental);
+
+
+        return $this->render('BackendBundle:Dental:create.html.twig', array('form' => $form->createView(),'edition' => true, 'id' => $id, 'horse' => $horse));
+    }
+
+    public function createDentalAction($horse)
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $params = $request->request->get('dental');
+        $horse = $this->get('rest.handler.model')->get('horses/'.$horse, 'horse', $session->get('access_token'));
+        $dental = $this->get('dental.handler.model')->postDental($horse, array('dental' => $request), $session->get('access_token'));
+
+        return $this->redirect($this->generateUrl('dentals_list', array('horse' => $horse['id'])));
+    }
+
+    public function putDentalAction($horse, $id)
+    {
+        $session = $this->getRequest()->getSession();
+        $params = $this->getRequest()->request->get('dental');
+        $horse = $this->get('rest.handler.model')->get('horses/'.$horse, 'horse', $session->get('access_token'));
+        $dental = $this->get('dental.handler.model')->putDental($horse, $id, array('dental' => $params), $session->get('access_token'));
+
+        return $this->redirect($this->generateUrl('dentals_list', array('horse' => $horse['id'])));
+    }
+
+    public function deleteDentalAction($horse, $id)
+    {
+        $session = $this->getRequest()->getSession();
+        $dental = $this->get('dental.handler.model')->deleteDental($horse, $id, $session->get('access_token'));
+
+        return $this->redirect($this->generateUrl('dentals_list', array('horse' => $horse)));
+    }
+}
---
+++ src/IHorse/BackendBundle/Form/DentalType.php
@@ -0,0 +1,22 @@
+<?php
+namespace IHorse\BackendBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DentalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('primaryReport', 'text', array('required' => false))
+            ->add('feeding', 'text', array('required' => false))
+            ->add('sedation', 'text', array('required' => false));
+    }
+
+    public function getName()
+    {
+        return 'dental';
+    }
+
+}
