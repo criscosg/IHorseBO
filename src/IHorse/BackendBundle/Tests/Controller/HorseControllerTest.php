9cc8dd9769fdbb109a715636dbc15187c68b3eb2
josegargon <josegargon2@gmail.com>
2014-02-03 08:09:41 +0100

    [Sprint 50] test horseController

--- src/IHorse/BackendBundle/Tests/Controller/HorseControllerTest.php
+++ src/IHorse/BackendBundle/Tests/Controller/HorseControllerTest.php
@@ -13,6 +13,7 @@ class HorseControllerTest extends CustomTestCase
     public function testAll()
     {
         $crawler = $this->client->request('GET', '/horses/');
+        $this->printContent();
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertEquals(0, $crawler->filter('html:contains("Test")')->count());
     }
